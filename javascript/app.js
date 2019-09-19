16890b1bbe557edff3d954fbe3d0c2c3b094be2e
lambertco <blambertco@gmail.com>
2019-09-19 16:13:07 -0600

    firebase database linked to javascript, slight styling added to css

---
+++ javascript/app.js
@@ -0,0 +1,15 @@
+  //Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyByLq7t_UeF1Pl_6zBKDwf0ba9oL_M4mZE",
+    authDomain: "testing-84e29.firebaseapp.com",
+    databaseURL: "https://testing-84e29.firebaseio.com",
+    projectId: "testing-84e29",
+    storageBucket: "",
+    messagingSenderId: "772947666733",
+    appId: "1:772947666733:web:cc8170d861205fc06f95a4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+  
